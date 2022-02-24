913798dd7c92d01b5cd70c030d834c4682d1fae5
khkim <khkim@gaia3d.com>
2022-02-24 09:34:54 +0900

    init

---
+++ src/main/java/com/example/h2_prac/H2PracApplication.java
@@ -0,0 +1,13 @@
+package com.example.h2_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2PracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(H2PracApplication.class, args);
+    }
+
+}
